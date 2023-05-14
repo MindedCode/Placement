2f1ee3fc8005d918da161c6968a67f5712f2c0ff
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-05-14 12:14:24 +0530

    Update k. StrongNum.java

--- k. StrongNum.java
+++ k. StrongNum.java
@@ -5,6 +5,26 @@
 *******************************************************************************/
 public class Main
 {
+    static int factR(int n){
+        
+        if(n == 0)
+        return 1;
+        return n * factR(n-1);
+    }
+    static void __strongN(int n){
+        
+        int sum = 0;
+        int temp = n;
+        // check the strong Number
+        while(temp!=0){
+            sum += factR(temp%10);
+            temp/=10;
+        }
+        if(temp == n)
+            System.out.println(n + " is Strong Number ");
+        else System.out.println(n + " is Strong Number ");
+        
+    }
     static boolean detectStrong(int n){
         int sum=0, temp=n; 
         boolean flag = false;
@@ -25,7 +45,7 @@ public class Main
 	    return sum;
 	}
 	
-	static int _strongN(int n){
+	static void _strongN(int n){
 	    int temp = n;
 	    int sum = 0;
 	    while(n!=0){
@@ -34,15 +54,15 @@ public class Main
 	        n/=10;
 	    }
 	    if(temp == sum){
-		    System.out.println("Yes Strong Number"); 
+		    System.out.println("Yes " + sum + " Strong Number"); 
 		} else {
-		    System.out.println("No"); return sum;
+		    System.out.println(sum + " No");
 		}
 	}
 	public static void main(String[] args) {
-		int num = 145;
-		System.out.println(_strongN(num));
-// 		__strong(num);
+		
+		_strongN(145);
+		__strongN(145);
 		
 		if(detectStrong(145))
         System.out.println("Yes");
