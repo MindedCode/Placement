fdc3fe5a3a469cb9c6c4f762e67e52d6397c8a0e
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-05-15 02:56:47 +0530

    Create l. perfectN.java

---
+++ l. perfectN.java
@@ -0,0 +1,21 @@
+public class Main
+ {
+   public static void main (String[]args)
+   {
+
+     int n = 28, sum = 0;
+
+     for (int i = 1; i < n; i++)
+       {
+     	if (n % i == 0){
+ 	        sum = sum + i;
+ 	        System.out.println(i +" "+ sum);}
+       }
+
+     if (sum == n)
+       System.out.println (n + " Is a perfect number");
+     else
+       System.out.println (n + " Is not a perfect number");
+
+   }
+ }
