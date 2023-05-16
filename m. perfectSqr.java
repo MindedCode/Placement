ed3dda47fcc900563e5fb556a21e6186a253c8a4
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-05-16 16:37:12 +0530

    Update m. perfectSqr.java

--- m. perfectSqr.java
+++ m. perfectSqr.java
@@ -5,7 +5,13 @@
 *******************************************************************************/
 
 public class Main
-{
+{   
+    public static void checkPerfectSqr(int n){
+        if(Math.ceil((double)Math.sqrt(n)) == Math.floor((double)Math.sqrt(n)))
+        System.out.println("True");
+        else System.out.println("False");
+        
+    }
     public static boolean isPerfect(int x){
         if(x>=0){
             int sr = (int)Math.sqrt(x);
@@ -18,6 +24,12 @@ public class Main
 		if(isPerfect(p))
 		    System.out.println("True");
 		else System.out.println("False");
+		checkPerfectSqr(p);
 		
 	}
 }
+
+
+// Output
+True
+True
