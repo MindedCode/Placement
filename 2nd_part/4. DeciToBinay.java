371fc47862283834407cb7378912c1cb29281694
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-06-29 22:07:16 +0530

    Update 4. DeciToBinay.java

--- 2nd_part/4. DeciToBinay.java
+++ 2nd_part/4. DeciToBinay.java
@@ -1,3 +1,15 @@
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+				
+    		Java program to convert decimal number to binary number
+       			1. Java program to convert decimal number to octal number
+	  `		2. JAVA program to convert decimal to octal number
+    			Decimal to Hexadecimal Conversion in Java
+       
+    
+*/
+
+
+
 //Java program to convert decimal number to binary number
 public class Main
 {
@@ -21,6 +33,8 @@ public class Main
 		System.out.print(binary[j]+"");
 	}
 }
+
+
 
 
 
@@ -137,7 +151,116 @@ public class Main {
 }
 
 
+// Explanation of output of the Program
+
+// 	int count = 1
+// 	octal = 0
+// 		condition => while(33!=0)
+// 		 remainder = 33%8 = 1
+// 		octal = octal(0) + ramin(1) * count(1) = 1
+// 		octal = 1
+// 		count =  count(1) * 10  => 10
+// 		decimal = 33/8 = 4
+
+// 		sout(octal) => 1
+// 		===========================
+
+// 		condition => while(4>0)
+// 		 remainder = 1%8 = 4
+// 		octal = octal(1) + ramin(4) * count(10) = 41
+// 		octal = 40
+// 		count =  count(10) * 10  => 100
+// 		decimal = 34/8 = 0
 
 
+// 		sout(octal) => 41     
+// 		=============================
+// 		false condition => while(0!=0)
+
+// ======================================================================
+// 		output is 41
+		
+
+
+
+
+
+Decimal to Hexadecimal Conversion in Java==============================================
+
+ public class Main 
+{
+  
+ 
+ 
+public static void main (String[]args) 
+  {
+    
+ 
+ 
+int decimal = 1457;
+    
+ 
+ 
+convert (decimal);
+  
+ 
+ 
+} 
+ 
+static void convert (int num) 
+  {
+    
+ 
+ 
+			// creating a char array to store hexadecimal equivalent
+    char[] hexa = new char[100];
+    
+ 
+      // using i to store hexadecimal bit at given array position
+    int i = 0;
+    
+while (num != 0)
+      
+      {
+    
+int rem = 0;
+  
+ 
+ 
+rem = num % 16;
+   
+ 
+       // check if rem < 10 : Digits : 0 - 9
+          // ascii 0 : 48
+        if (rem < 10)
+          {
+        hexa[i] = (char) (rem + 48);
+           i++;
+         }                    
+         // else positional values : A - F 
+         // rem value will be > 10, adding 55 will result : A - F 
+        // ascii A : 65, B : 66 ..... F : 70 
+      else
+     
+       {
+        
+hexa[i] = (char) (rem + 55);
+          
+i++;
+        
+} 
+num = num / 16;
+      
+} 
+// printing hexadecimal array in reverse order
+      System.out.println ("Hexadecimal:");
+    
+for (int j = i - 1; j >= 0; j--)
+      
+System.out.print (hexa[j]);
+
+} 
+ 
+} 
 
 
