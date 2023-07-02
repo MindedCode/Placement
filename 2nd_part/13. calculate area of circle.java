19161f670098c3345c08de02cc500af9a151f566
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-02 15:35:16 +0530

    Create 13. calculate area of circle.java

---
+++ 2nd_part/13. calculate area of circle.java
@@ -0,0 +1,44 @@
+
+/*
+          Mwthod 1: Simple
+          Methed 2: help of diameter
+
+*/
+
+
+//Java program to calculate area of a circle
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+			
+		double radius = 7;			
+		
+        //formula for area of a circle
+		double area = 3.14 * radius * radius;		
+		System.out.println(area);
+		
+	}
+}
+
+
+
+//Java program to calculate area of a circle help of diameter======================
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+			
+		double d = 14;			
+		
+                //formula for area of a circle
+		double area = (3.14 * d * d)/4;		
+		System.out.println(area);
+		
+	}
+}
+
+// Output is :
+// 153.86
