ec2bbb46e44cd310a1659202f85a2911ebabafde
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-02 18:25:01 +0530

    Create d. LCM.java

---
+++ 3rd part/d. LCM.java
@@ -0,0 +1,18 @@
+class Main {
+    // Recursive method to return gcd of a and b
+    static int gcd(int a, int b) {
+        if (a == 0) return b;
+        return gcd(b % a, a);
+    }
+    // method to return LCM of two numbers
+    static int lcm(int a, int b) {
+        return (a / gcd(a, b)) * b;
+    }
+    // Driver method
+    public
+    static void main(String[] args) {
+        int a = 15, b = 20;
+        System.out.println("LCM of " + a + " and " + b + " is " + lcm(a, b));
+    }
+}
+LCM of 15 and 20 is 60
